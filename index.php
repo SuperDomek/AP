f393560d41bfe267e2be878455ef81f7cb60fd59
Dominik Bláha <blahad@sic.czu.cz>
2018-04-13 16:00:16 +0200

    Export do Excelu

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
 // Initialize global environment
 define('INDEX_FILE_LOCATION', __FILE__);
 require('lib/pkp/includes/bootstrap.inc.php');
-require Core::getBaseDir() . '\vendor\autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 // Serve the request
 $application =& PKPApplication::getApplication();
